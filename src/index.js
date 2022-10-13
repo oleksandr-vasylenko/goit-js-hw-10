bc6b9929137ba1a795ded804da3d74c8a759c24d
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-10-13 21:46:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const fieldRef = document.querySelector('#search-box');
+const listRef = document.querySelector('.country-list');
+const infoRef = document.querySelector('.country-info');
+
+fieldRef = addEventListener('input', onInput);
+
+function onInput(e) {
+  let searchText = e.target.value;
+
+  function fetchCountries() {
+    fetch(`https://restcountries.com/v3.1/name/${searchText}`)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+      });
+  }
+}
