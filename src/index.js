8c2c60d076308e8d058f5d65cf9a4a6e2388c530
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-10-14 21:59:45 +0300

    Completes the requirements

--- src/index.js
+++ src/index.js
@@ -36,10 +36,8 @@ function renderMarkup(foundData) {
   } else if (foundData.length === 0) {
     onError(error);
   } else if (foundData.length <= 10 && foundData.length > 1) {
-    console.log('should be short markup');
     listRef.insertAdjacentHTML('beforeend', shortMarkup(foundData));
   } else if (foundData.length === 1) {
-    console.log('should be full markup');
     InfoRef.insertAdjacentHTML('beforeend', fullMarkup(foundData));
   }
 }
@@ -47,15 +45,3 @@ function renderMarkup(foundData) {
 function onError() {
   Notiflix.Notify.failure('Oops, there is no country with that name');
 }
-
-// function onInput(e) {
-//   let searchText = e.target.value.trim().toLowerCase();
-//   if (searchText !== '') {
-//     fetchCountries().then(data => {
-//       data.filter(item => {
-//         item.name.official.toLowerCase().includes(searchText);
-//         renderMarkup();
-//       });
-//     });
-//   }
-// }
